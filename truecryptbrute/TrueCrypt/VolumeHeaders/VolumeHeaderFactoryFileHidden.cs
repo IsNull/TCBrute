3885aefcd95e39839f0882f74374f9f9fad3f628
IsNull <IsNull@securityvision.ch>
2010-12-11 12:58:14 +0100

    fixed HiddenVolume Offset

--- truecryptbrute/TrueCrypt/VolumeHeaders/VolumeHeaderFactoryFileHidden.cs
+++ truecryptbrute/TrueCrypt/VolumeHeaders/VolumeHeaderFactoryFileHidden.cs
@@ -11,7 +11,7 @@ namespace truecryptbrute.TrueCrypt.VolumeHeaders
             : base(uvolumePath) { }
 
         public override VolumeHeader ReadEncryptedVolumeHeader() {
-            return ReadVolumeHeaderEncrypted(512);
+            return ReadVolumeHeaderEncrypted(64 * 1024);
         }
     }
 }
